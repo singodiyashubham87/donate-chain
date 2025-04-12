246ae28513ad7ba19adcb4d0f3f098b3c0fc7d00
Shubham Singodiya <singodiyashubham87@gmail.com>
2025-04-13 00:45:07 +0530

    chore: update integrate-wallet

--- components/integrate-wallet.tsx
+++ components/integrate-wallet.tsx
@@ -4,21 +4,45 @@ import {
   ConnectWallet,
   Wallet,
   WalletDropdown,
+  WalletDropdownLink,
   WalletDropdownDisconnect,
 } from "@coinbase/onchainkit/wallet";
-import { Address, Identity, EthBalance } from "@coinbase/onchainkit/identity";
+import {
+  Address,
+  Avatar,
+  Name,
+  Identity,
+  EthBalance,
+} from "@coinbase/onchainkit/identity";
 
 export default function IntegrateWallet() {
   return (
-    <Wallet>
-      <ConnectWallet/>
-      <WalletDropdown className="w-full bg-green-600">
-        <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick>
-          <Address />
-          <EthBalance />
-        </Identity>
-        <WalletDropdownDisconnect />
-      </WalletDropdown>
-    </Wallet>
+    <div className="flex justify-end">
+      <div className="wallet-container">
+        <Wallet>
+          <ConnectWallet>
+            <Avatar className="h-6 w-6" />
+            <Name />
+          </ConnectWallet>
+          <WalletDropdown>
+            <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick>
+              <Avatar />
+              <Name />
+              <Address />
+              <EthBalance />
+            </Identity>
+            <WalletDropdownLink
+              icon="wallet"
+              href="https://keys.coinbase.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Wallet
+            </WalletDropdownLink>
+            <WalletDropdownDisconnect />
+          </WalletDropdown>
+        </Wallet>
+      </div>
+    </div>
   );
 }
