ad6c2e67b134078c8202e84c08ee68ac9f6c4c2e
Shubham Singodiya <singodiyashubham87@gmail.com>
2025-04-12 14:28:55 +0530

    chore: add auth0

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,8 +3,16 @@
 import { base } from "wagmi/chains";
 import { OnchainKitProvider } from "@coinbase/onchainkit";
 import { ThemeProvider } from "next-themes";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const AUTH0_DOMAIN = process.env.NEXT_PUBLIC_AUTH0_DOMAIN;
+  const AUTH0_CLIENT_ID = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID;
+  const AUTH0_REDIRECT_URI = process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI;
+
+  if (!AUTH0_CLIENT_ID || !AUTH0_DOMAIN || !AUTH0_REDIRECT_URI) {
+    throw new Error("Environment variables for Auth0 are not defined");
+  }
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <OnchainKitProvider
@@ -16,7 +24,15 @@ export function Providers({ children }: { children: React.ReactNode }) {
           },
         }}
       >
-        {children}
+        <Auth0Provider
+          domain={AUTH0_DOMAIN}
+          clientId={AUTH0_CLIENT_ID}
+          authorizationParams={{
+            redirect_uri: AUTH0_REDIRECT_URI,
+          }}
+        >
+          {children}
+        </Auth0Provider>
       </OnchainKitProvider>
     </ThemeProvider>
   );
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,14 +1,20 @@
-'use client';
+"use client";
 
-import * as React from 'react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { cn } from '@/lib/utils';
-import { Button } from './ui/button';
-import { ThemeToggle } from './theme-toggle';
+import * as React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Loader } from "lucide-react";
+import Image from "next/image";
 
 export function MainNav() {
+  const { user, isAuthenticated, isLoading, loginWithRedirect, logout } =
+    useAuth0();
   const pathname = usePathname();
+
+  if (isLoading) return <Loader />;
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
@@ -21,8 +27,8 @@ export function MainNav() {
             <Link
               href="/ngos"
               className={cn(
-                'transition-colors hover:text-foreground/80',
-                pathname === '/ngos' ? 'text-foreground' : 'text-foreground/60'
+                "transition-colors hover:text-foreground/80",
+                pathname === "/ngos" ? "text-foreground" : "text-foreground/60"
               )}
             >
               NGOs
@@ -30,8 +36,10 @@ export function MainNav() {
             <Link
               href="/about-us"
               className={cn(
-                'transition-colors hover:text-foreground/80',
-                pathname === '/about-us' ? 'text-foreground' : 'text-foreground/60'
+                "transition-colors hover:text-foreground/80",
+                pathname === "/about-us"
+                  ? "text-foreground"
+                  : "text-foreground/60"
               )}
             >
               About
@@ -39,19 +47,42 @@ export function MainNav() {
             <Link
               href="/contact-us"
               className={cn(
-                'transition-colors hover:text-foreground/80',
-                pathname === '/contact' ? 'text-foreground' : 'text-foreground/60'
+                "transition-colors hover:text-foreground/80",
+                pathname === "/contact-us"
+                  ? "text-foreground"
+                  : "text-foreground/60"
               )}
             >
               Contact
             </Link>
           </nav>
         </div>
-        <div className="flex items-center space-x-4">
-          <ThemeToggle />
-          <Button asChild>
-            <Link href="/ngos">Start Donating</Link>
-          </Button>
+        <div className="flex gap-6 items-center">
+          {!isAuthenticated ? (
+            <Button onClick={() => loginWithRedirect()}>Login</Button>
+          ) : (
+            <React.Fragment>
+              <Image
+                src={user?.picture || "/default-avatar.png"}
+                alt={user?.name || "User Avatar"}
+                width={32}
+                height={32}
+                className="h-8 w-8 rounded-full"
+              />
+              <Button
+                variant="outline"
+                onClick={() =>
+                  logout({
+                    logoutParams: {
+                      returnTo: window.location.origin,
+                    },
+                  })
+                }
+              >
+                Logout
+              </Button>
+            </React.Fragment>
+          )}
         </div>
       </div>
     </header>
