350c285b311b2b288b57362fcbe3ef7099222bf5
Shubham Singodiya <singodiyashubham87@gmail.com>
2025-04-12 15:02:00 +0530

    chore: minor fix

--- components/integrate-wallet.tsx
+++ components/integrate-wallet.tsx
@@ -11,7 +11,7 @@ import { Address, Identity, EthBalance } from "@coinbase/onchainkit/identity";
 export default function IntegrateWallet() {
   return (
     <Wallet>
-      <ConnectWallet></ConnectWallet>
+      <ConnectWallet/>
       <WalletDropdown className="w-full bg-green-600">
         <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick>
           <Address />
