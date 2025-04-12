fcf8c3e4dbd7a246d9726814d91d890a0299acb2
Shubham Singodiya <singodiyashubham87@gmail.com>
2025-04-13 01:13:41 +0530

    update next config, add missing packages

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
