0f0382d3600a37ca0882b7e2d370198be5c33ba7
Shubham Singodiya <singodiyashubham87@gmail.com>
2025-04-13 19:23:15 +0530

    chore: minor ui fix

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,6 +3,7 @@
 import { base } from "wagmi/chains";
 import { OnchainKitProvider } from "@coinbase/onchainkit";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const AUTH0_DOMAIN = process.env.NEXT_PUBLIC_AUTH0_DOMAIN;
@@ -13,24 +14,27 @@ export function Providers({ children }: { children: React.ReactNode }) {
     throw new Error("Environment variables for Auth0 are not defined");
   }
   return (
-    <Auth0Provider
-      domain={AUTH0_DOMAIN}
-      clientId={AUTH0_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: AUTH0_REDIRECT_URI,
-      }}
-    >
-      <OnchainKitProvider
-        apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-        chain={base}
-        config={{
-          appearance: {
-            mode: "auto",
-          },
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      <Auth0Provider
+        domain={AUTH0_DOMAIN}
+        clientId={AUTH0_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: AUTH0_REDIRECT_URI,
         }}
       >
-        {children}
-      </OnchainKitProvider>
-    </Auth0Provider>
+        <OnchainKitProvider
+          apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+          chain={base}
+          config={{
+            appearance: {
+              mode: "light",
+              theme: "hacker",
+            },
+          }}
+        >
+          {children}
+        </OnchainKitProvider>
+      </Auth0Provider>
+    </ThemeProvider>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  darkMode: ['class'],
+  darkMode: ['class'], 
+  safelist: ['dark'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
